17358e0f3448416d02fc6206643907452ae7d600
MonkeyJuggler <peterjelewis@gmail.com>
2022-03-16 10:10:59 +0000

    dogyears

---
+++ dogyears.js
@@ -0,0 +1,23 @@
+
+// My age as a variable
+const myAge = 36;
+
+// First 2 years of the dogs life
+let earlyYears = 2;
+
+earlyYears = earlyYears * 10.5;
+
+//subtract the early years from myAge
+let laterYears = myAge - 2;
+
+//multiply laterYears by 4
+laterYears = laterYears * 4
+
+//full calculation of my age in dog years
+let myAgeInDogYears = earlyYears + laterYears;
+
+// My name reduced to lower case
+const myName = "Peter".toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
+
