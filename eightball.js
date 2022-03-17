9172cc2cd70334e6087f823e7c6391285d7b4ae0
MonkeyJuggler <peterjelewis@gmail.com>
2022-03-17 11:06:43 +0000

    1st commit

---
+++ eightball.js
@@ -0,0 +1,48 @@
+let userName = "Peter";
+
+userName === undefined ? console.log("Hello!") : console.log(`Hello ${userName}`);
+
+let userQuestion = "Will I win the lottery?";
+
+console.log(`${userQuestion}`);
+
+let randomNumber = Math.floor(Math.random() * 8);
+
+let eightBall = "";
+
+switch(randomNumber) {
+case 1:
+console.log("It is certain");
+break
+case 2:
+console.log("It is decidedly so");
+break
+case 3:
+console.log("Reply hazy try again");
+break
+case 4:
+console.log("Cannot predict now");
+break
+case 5:
+console.log("Do not count on it");
+break
+case 6:
+console.log("My sources say no");
+break
+case 7:
+console.log("Outlook not so good");
+break
+case 8:
+console.log("Signs point to yes");
+break
+default:
+console.log("Shake again");
+break
+}
+  
+
+
+
+
+
+
